159169f7ec22719d66591d3370939792f9111241
Trung Vu <vu.trung.96@gmail.com>
2024-09-18 17:54:15 +0000

    formatting changes

--- dcft/dataset/annotators/__init__.py
+++ dcft/dataset/annotators/__init__.py
@@ -29,7 +29,7 @@ class AnnotatorConfig:
             self.max_requests_per_minute = args.max_requests_per_minute
             self.max_tokens_per_minute = args.max_tokens_per_minute
             self.batch = args.batch
-            self.max_batch_api_chunk_size = 100 
+            self.max_batch_api_chunk_size = 100
 
 
 def is_gpt_annotator(annotator_name):

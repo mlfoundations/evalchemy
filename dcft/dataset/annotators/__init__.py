488166ac324c5aa16bcee134edbe8b566cf2188f
Ryan Marten <ryanmarten2000@gmail.com>
2024-09-18 09:07:03 -0700

    add cotalpaca and smaller batches of 1k (can go smaller)

--- dcft/dataset/annotators/__init__.py
+++ dcft/dataset/annotators/__init__.py
@@ -29,7 +29,8 @@ class AnnotatorConfig:
             self.max_requests_per_minute = args.max_requests_per_minute
             self.max_tokens_per_minute = args.max_tokens_per_minute
             self.batch = args.batch
-            self.max_batch_api_chunk_size = 50000
+            # self.max_batch_api_chunk_size = 50000
+            self.max_batch_api_chunk_size = 1000 # trying for smaller batches to see if completion time is faster
 
 
 def is_gpt_annotator(annotator_name):
--- dcft/dataset/hf/__init__.py
+++ dcft/dataset/hf/__init__.py
@@ -1,11 +1,13 @@
 from dcft.dataset.hf.codealpaca import CodeAlpacaFTDataset
 from dcft.dataset.hf.codeglaive import CodeGlaiveFTDataset
 from dcft.dataset.hf.platypus import PlatypusFTDataset
+from dcft.dataset.hf.cotalpaca import COTAlpacaFTDataset
 
 dataset_map = {
     "sahil2801/CodeAlpaca-20k": CodeAlpacaFTDataset,
     "glaiveai/glaive-code-assistant": CodeGlaiveFTDataset,
     "garage-bAInd/Open-Platypus": PlatypusFTDataset,
+    "causal-lm/cot_alpaca_gpt4": COTAlpacaFTDataset,  # Add the appropriate identifier for the dataset
 }
 
 

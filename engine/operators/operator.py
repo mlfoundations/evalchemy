7513d430a9e40305230687d87e5ba5951372bdae
Trung Vu <vu.trung.96@gmail.com>
2024-09-29 15:55:00 +0000

    restruct operator.py

--- engine/operators/operator.py
+++ engine/operators/operator.py
@@ -1,23 +1,42 @@
 from abc import ABC, abstractmethod
+from typing import Dict, List, Type, TypeAlias
 
-from typing import Dict, List, TypeAlias, Callable, Type
-import importlib
-import logging
 import ray
-from lm_eval.utils import eval_logger
-from datasets import Dataset, concatenate_datasets, load_dataset
-
-from engine.operators.configs import (
-    OperatorSpecificConfig,
-    OperatorConfig,
-    FunctionOperatorConfig,
-    HFSourceOperatorConfig,
-    CONFIG_TYPE_MAP,
-)
+from pydantic import BaseModel, Field
 
 ShardRef: TypeAlias = ray.ObjectRef
 ManyShardRefs: TypeAlias = List[ShardRef]
 DatasetRefs: TypeAlias = Dict[str, ManyShardRefs]
+
+
+class OperatorSpecificConfig(BaseModel):
+    """
+    Base class for operator-specific configurations.
+
+    Attributes:
+        type (str): The type of the operator.
+    """
+    type: str
+
+
+class OperatorConfig(BaseModel):
+    """
+    Configuration class for operators.
+
+    Attributes:
+        id (str): Unique identifier for the operator.
+        input_ids (List[str]): List of input identifiers for the operator.
+        config (OperatorSpecificConfig): Specific configuration for the operator.
+
+    Config:
+        extra (str): Set to "forbid" to disallow extra attributes.
+    """
+    id: str
+    input_ids: List[str] = Field(default_factory=list)
+    config: OperatorSpecificConfig
+
+    class Config:
+        extra = "forbid"
 
 
 class Operator(ABC):
@@ -59,9 +78,13 @@ class Operator(ABC):
             inputs (DatasetRefs): Dictionary of inputs mapping identifiers to a list of shard references (known as a dataset)
 
         Returns:
-            ManyShardRefs: List of processedoutput shard references for each input shard
+            ManyShardRefs: List of processed output shard references for each input shard
         """
         pass
+
+
+OPERATOR_MAP: Dict[Type[OperatorSpecificConfig], Type[Operator]] = {}
+CONFIG_TYPE_MAP: Dict[str, Type[OperatorSpecificConfig]] = {}
 
 
 def create_operator(config: OperatorConfig) -> Operator:
@@ -81,185 +104,6 @@ def create_operator(config: OperatorConfig) -> Operator:
     if operator_class is None:
         raise ValueError(f"Unknown operator type: {type(config.config)}")
     return operator_class(config.id, config.input_ids, config.config)
-
-
-class FunctionOperator(Operator):
-    """
-    Operator that applies a function to the input dataset or shard.
-
-    Attributes:
-        function (Callable[[Dataset], Dataset]): The function to apply to the dataset or shard (shard of a dataset is a dataset).
-        function_config (Dict[str, Any]): Additional configuration for the function.
-        num_shards (int): Number of shards to split the dataset into if the function can operate across individual shards
-        sharded (bool): If the function can be applied to individual shards of a dataset rather than the whole, set this to true to utilize parallelism
-    """
-
-    def __init__(self, id: str, input_ids: List[str], config: FunctionOperatorConfig):
-        """
-        Initialize the FunctionOperator.
-
-        Args:
-            id (str): Unique identifier for the operator.
-            input_ids (List[str]): List of input identifiers for the operator.
-            config (FunctionOperatorConfig): Specific configuration for the function operator.
-        """
-        super().__init__(id, input_ids, config)
-        self.function = self._load_function(config.function)
-        self.function_config = config.function_config
-        self.num_shards = config.num_shards
-        self.sharded = config.sharded
-
-    def _load_function(self, function_path: str) -> Callable[[Dataset], Dataset]:
-        """
-        Load the function from the given path.
-
-        Args:
-            function_path (str): Path to the function.
-
-        Returns:
-            Callable[[Dataset], Dataset]: The loaded function.
-        """
-        module_name, function_name = function_path.rsplit(".", 1)
-        module = importlib.import_module(module_name)
-        return getattr(module, function_name)
-
-    def execute(self, inputs: DatasetRefs) -> ManyShardRefs:
-        """
-        Execute the function operator on the input datasets.
-
-        Args:
-            inputs (DatasetRefs): Map of input datasets to apply function on
-
-        Returns:
-            ManyShardRefs: List of shards outputted by the function for each input
-        """
-        outputs = []
-        input_shards = []
-        for input in inputs.values():
-            input_shards.extend(input)
-        # Sharding stage
-        if self.sharded and len(input_shards) == 1:
-            shards_to_process = self.shard_dataset.remote(self, input_shards[0])
-        elif not self.sharded and len(input_shards) > 1:
-            shards_to_process = [self.merge_shards.remote(self, input_shards)]
-        else:
-            shards_to_process = input_shards
-
-        # Processing stage: if dataset is sharded, then shards_to_process contains multiple shards
-        # otherwise, it contains a single dataset.
-
-        for shard in shards_to_process:
-            processed_dataset = self.process_shard.remote(self, shard)
-            outputs.append(processed_dataset)
-        return outputs
-
-    @ray.remote
-    def shard_dataset(self, dataset: Dataset) -> ray.ObjectRefGenerator:
-        """
-        Shard the input dataset into multiple parts to utilize parallelism.
-
-        Args:
-            dataset (Dataset): The input dataset to be sharded.
-
-        Returns:
-            ray.ObjectRefGenerator: Generator of dataset shards.
-        """
-        # TODO: implement
-        total_size = len(dataset)
-        split_size = total_size // self.num_shards
-        remainder = total_size % self.num_shards
-
-        # Create the splits
-        start = 0
-        for i in range(self.num_shards):
-            end = start + split_size + (1 if i < remainder else 0)
-            split = dataset.select(range(start, end))
-            start = end
-            yield split
-
-    @ray.remote
-    def merge_shards(self, shards: ManyShardRefs) -> Dataset:
-        """
-        Merge multiple dataset shards into a single dataset if function requires all data at once.
-
-        Args:
-            shards (ManyShardRefs): List of dataset shard references.
-
-        Returns:
-            Dataset: Merged dataset.
-        """
-        return concatenate_datasets([ray.get(shard) for shard in shards])
-
-    @ray.remote
-    def process_shard(self, dataset: Dataset) -> Dataset:
-        """
-        Process a single dataset or single shard (a shard is a dataset) using the configured function.
-
-        Args:
-            dataset (Dataset): The input dataset or single shard (a shard is a dataset)
-
-        Returns:
-            Dataset: Processed dataset or shard.
-        """
-        logging.info(f"Processing shard with function: {self.function.__name__}")
-        return self.function(dataset, **self.function_config)
-
-
-class HFSourceOperator(Operator):
-    """
-    Operator that loads a dataset from Hugging Face's datasets library.
-
-    Attributes:
-        dataset (str): Name of the dataset to load.
-        split (str): The split of the dataset to use.
-        columns (Optional[List[str]]): Specific columns to load from the dataset.
-        num_truncate (Optional[int]): Number of samples to truncate the dataset to.
-    """
-
-    def __init__(self, id: str, input_ids: List[str], config: HFSourceOperatorConfig):
-        """
-        Initialize the HFSourceOperator.
-
-        Args:
-            id (str): Unique identifier for the operator.
-            input_ids (List[str]): List of input identifiers for the operator.
-            config (HFSourceOperatorConfig): Specific configuration for the HF source operator.
-        """
-
-        super().__init__(id, input_ids, config)
-        self.dataset = config.dataset
-        self.split = config.split
-        self.columns = config.columns
-        self.num_truncate = config.num_truncate
-
-    def execute(self, _: DatasetRefs) -> ManyShardRefs:
-        """
-        Execute the HF source operator to load the dataset.
-
-        Args:
-            _ (DatasetRefs): Unused input (for compatibility with the Operator interface).
-
-        Returns:
-            ManyShardRefs: List containing a single reference to the loaded dataset.
-        """
-        dataset = self.load_dataset()
-        return [ray.put(dataset)]
-
-    def load_dataset(self) -> Dataset:
-        """
-        Load the dataset from Hugging Face's datasets library.
-
-        Returns:
-            Dataset: The loaded and potentially processed dataset.
-        """
-        dataset = load_dataset(self.dataset, split=self.split)
-        if self.columns:
-            dataset = dataset.select_columns(self.columns)
-        if self.num_truncate is not None:
-            dataset = dataset.select(range(min(len(dataset), self.num_truncate)))
-        eval_logger.info(f"\nDataset loaded from {self.dataset}:")
-        eval_logger.info(dataset)
-        return dataset
 
 
 def register_operator(config_class: Type[OperatorSpecificConfig], operator_class: Type[Operator]):
@@ -287,7 +131,15 @@ def get_operator_class(config_class: Type[OperatorSpecificConfig]) -> Type[Opera
     return OPERATOR_MAP.get(config_class)
 
 
-OPERATOR_MAP: Dict[Type[OperatorSpecificConfig], Type[Operator]] = {}
+def get_config_class(config_type: str) -> Type[OperatorSpecificConfig]:
+    """
+    Get the configuration class for a given operator type.
 
-register_operator(HFSourceOperatorConfig, HFSourceOperator)
-register_operator(FunctionOperatorConfig, FunctionOperator)
+    Args:
+        config_type (str): The type of the operator configuration.
+
+    Returns:
+        Type[OperatorSpecificConfig]: The corresponding configuration class for the given type.
+        If the type is not found in the CONFIG_TYPE_MAP, returns None.
+    """
+    return CONFIG_TYPE_MAP.get(config_type)

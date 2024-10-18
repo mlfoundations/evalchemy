0434d82f211bb14770545b601394a5d320711ad9
Negin Raoof <neginmr@utexas.edu>
2024-10-17 17:41:52 -0700

    Update config.py

--- database/config.py
+++ database/config.py
@@ -5,9 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-
+DB_HOST = "35.225.163.235"
+DB_PORT = "5432"
+DB_NAME = "postgres"
+DB_USER = "postgres"
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"

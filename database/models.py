e8016202235606c54e00316eb3c73678d912da3f
sedrick-keh-tri <sedrick.keh@tri.global>
2024-10-24 06:29:23 +0000

    make database schemas printable

--- database/models.py
+++ database/models.py
@@ -20,6 +20,13 @@ class Dataset(Base):
     generation_parameters = Column(JSONB, nullable=False)
     dataset_type = Column(Text, nullable=False)
 
+    def __repr__(self):
+        return (
+            f"Dataset(id={self.id}, name={self.name}, created_by={self.created_by}, "
+            f"creation_location={self.creation_location}, creation_time={self.creation_time}, "
+            f"data_location={self.data_location}, dataset_type={self.dataset_type})"
+        )
+
 
 class Model(Base):
     __tablename__ = "models"
@@ -38,6 +45,15 @@ class Model(Base):
     weights_location = Column(Text, nullable=False)
     wandb_link = Column(Text, nullable=True)
 
+    def __repr__(self):
+        return (
+            f"Model(id={self.id}, name={self.name}, base_model_id={self.base_model_id}, "
+            f"created_by={self.created_by}, creation_location={self.creation_location}, "
+            f"training_start={self.training_start}, training_end={self.training_end}, "
+            f"training_status={self.training_status}, dataset_id={self.dataset_id}, "
+            f"is_external={self.is_external}, weights_location={self.weights_location})"
+        )
+
 
 class EvalResult(Base):
     __tablename__ = "evalresults"
@@ -52,6 +68,13 @@ class EvalResult(Base):
     creation_location = Column(Text, nullable=False)
     completions_location = Column(Text, nullable=False)
 
+    def __repr__(self):
+        return (
+            f"EvalResult(id={self.id}, model_id={self.model_id}, eval_setting_id={self.eval_setting_id}, "
+            f"score={self.score}, dataset_id={self.dataset_id}, created_by={self.created_by}, "
+            f"creation_time={self.creation_time}, completions_location={self.completions_location})"
+        )
+
 
 class EvalSetting(Base):
     __tablename__ = "evalsettings"
@@ -60,3 +83,9 @@ class EvalSetting(Base):
     name = Column(Text, nullable=False)
     parameters = Column(JSONB, nullable=False)
     eval_version_hash = Column(CHAR(64))
+
+    def __repr__(self):
+        return (
+            f"EvalSetting(id={self.id}, name={self.name}, parameters={self.parameters}, "
+            f"eval_version_hash={self.eval_version_hash})"
+        )

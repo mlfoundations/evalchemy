b59740744df4107fd3a9fee005d7b9993bb55bf1
Ryan Marten <ryanmarten2000@gmail.com>
2025-04-25 21:46:53 -0700

    update launch

--- eval/distributed/launch.py
+++ eval/distributed/launch.py
@@ -151,8 +151,8 @@ def check_conda_env(watchdog=False):
         activate_cmd = "source /work/08134/negin/anaconda3/bin/activate evalchemy"
         print_info(f"Detected TACC environment, checking python path: {python_path}")
     elif "jureca" in hostname:
-        python_path = "/work/08134/negin/anaconda3/envs/evalchemy/bin/python3.10"
-        activate_cmd = "source /work/08134/negin/anaconda3/bin/activate evalchemy"
+        python_path = "/p/data1/mmlaion/dcft/evalchemy_env/bin/python3.10"
+        activate_cmd = "source /p/data1/mmlaion/dcft/mamba/bin/activate /p/data1/mmlaion/dcft/evalchemy_env"
         print_info(f"Detected JURECA environment, checking python path: {python_path}")
     else:
         raise ValueError(f"Unknown hostname: {hostname}, can't determine which HF_HUB_CACHE to use")
@@ -324,6 +324,13 @@ def launch_sbatch(
         else:
             sbatch_script = "eval/distributed/process_shards_tacc.sbatch"
             print_info("Detected TACC environment, using process_shards_tacc.sbatch")
+    elif "jureca" in hostname:
+        if tp4:
+            sbatch_script = "eval/distributed/process_shards_jureca_tp.sbatch"
+            print_info("Detected JURECA environment with TP4, using process_shards_jureca_tp.sbatch")
+        else:
+            sbatch_script = "eval/distributed/process_shards_jureca.sbatch"
+            print_info("Detected JURECA environment, using process_shards_jureca.sbatch")
     else:
         raise ValueError(f"Unknown hostname: {hostname}, can't determine which sbatch script to use")
 

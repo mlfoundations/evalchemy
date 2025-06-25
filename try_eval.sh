73c3a32c5b4baa3a7d77a7afab799c744546aff0
thakicloud-kimdaeseong <daeseong.kim@thakicloud.co.kr>
2025-06-25 15:12:52 +0900

    새로운 real_eval.sh 파일 추가 및 try_eval.sh 파일 수정. real_eval.sh에서 다양한 평가 작업을 위한 모델 실행 스크립트를 추가하고, try_eval.sh에서 AMC23 작업에 대한 평가 설정을 업데이트하여 출력 경로와 최대 토큰 수를 조정함.

--- try_eval.sh
+++ try_eval.sh
@@ -1,10 +1,18 @@
+# python -m eval.eval \
+#     --model curator \
+#     --tasks GPQADiamond \
+#     --model_args "base_url=http://10.7.60.89:10001/v1,model=qwen3-8b,tokenizer=Qwen/Qwen3-8B,tokenizer_backend=huggingface,tokenized_requests=False,max_requests_per_minute=60,max_tokens_per_minute=50000" \
+#     --num_fewshot 0 \
+#     --batch_size 4 \
+#     --limit 1 \
+#     --apply_chat_template False \
+#     --max_tokens 8000 \
+#     --output_path logs/GPQADiamond_results_test          
+
 python -m eval.eval \
     --model curator \
-    --tasks MMLUPro \
+    --tasks AMC23 \
     --model_args "base_url=http://10.7.60.89:10001/v1,model=qwen3-8b,tokenizer=Qwen/Qwen3-8B,tokenizer_backend=huggingface,tokenized_requests=False,max_requests_per_minute=60,max_tokens_per_minute=50000" \
-    --num_fewshot 3 \
-    --batch_size 4 \
-    --limit 1 \
-    --apply_chat_template False \
-    --max_tokens 8000 \
-    --output_path logs/MMLUPro_results_test          
+    --limit 3 \
+    --max_tokens 200 \
+    --output_path logs/AMC23_results_test
